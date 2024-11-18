95dd60c78fe9b1cb60a27d0df9b2ad14f00c9dae
Samed Yumukhov <yumukhovsamed@gmail.com>
2024-11-18 18:11:55 +0300

    commit

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,8 @@
 from typing import Iterable, Any
 
 
-def filter_by_state(dictionaries: Iterable[list[dict[Any, Any]]], state: Any= 'EXECUTED') -> list[list[dict[Any, Any]]]:
+def filter_by_state(dictionaries: Iterable[list[dict[Any, Any]]], state: Any = 'EXECUTED') -> list[
+    list[dict[Any, Any]]]:
     """
     Функция принимает на вход список словарей и значение для ключа и возвращает новый
     список содержащий только те словари у которых ключ содержит переданное в функцию
@@ -13,7 +14,8 @@ def filter_by_state(dictionaries: Iterable[list[dict[Any, Any]]], state: Any= 'E
             executed_list.append(i)
     return executed_list
 
-def sort_by_date(dictioanaris:Iterable[list[dict[Any, Any]]], reverse: bool=True) ->  list[list[dict[Any, Any]]]:
+
+def sort_by_date(dictioanaris: Iterable[list[dict[Any, Any]]], reverse: bool = True) -> list[list[dict[Any, Any]]]:
     """
     Функия по сортировкам по датам
     """
