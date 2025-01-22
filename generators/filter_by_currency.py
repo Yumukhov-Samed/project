7d80cedc25e2dfad806e39d913fcc1add07e3cf0
Yumukhov-Samed <yumukhovsamed@gmail.com>
2025-01-22 15:24:42 +0300

    Доработаны все функции

--- generators/filter_by_currency.py
+++ generators/filter_by_currency.py
@@ -1,38 +1,12 @@
 from typing import Union
 
 
-def filter_by_currency(transaction_list: Union[list, dict], currency: str) -> Union[list, dict]:
+def filter_by_currency(transactions: Union[list, dict], currency: str) -> Union[list, dict]:
     """Функция должна возвращать итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной """
 
-    filter(lambda x: x['currency']['code'] == currency, transaction_list)
-
-    return transaction_list
-
-usd_transactions = filter_by_currency
-for _ in range(2):
-    print(usd_transactions)
-
-
-def transaction_descriptions(list_of_transactions: Union[list, dict]) -> None:
-    """генератор, который принимает список словарей с транзакциями и возвращает описание каждой операции по очереди."""
-    id_transaction = filter(lambda x: x['id'] is True, list_of_transactions)
-    description_transaction = (filter(lambda x: x['description'] is True, list_of_transactions))
-    while True:
-        print(id_transaction['id'])
-        yield description_transaction['description']
-descriptions = transaction_descriptions
-for _ in range(5):
-    print(descriptions)
-
-def card_number_generator(start, end):
-    for i in range(start, end + 1):
-        card_number = '{:0>16}'.format(str(i))
-        card_number = ' '.join([card_number[i:i + 4] for i in range(0, len(card_number), 4)])
-        yield card_number
-
-for card_number in card_number_generator(1, 5):
-    print(card_number)
-
+    for transaction in transactions:
+        if transaction['operationAmount']['currency']['code'] == currency:
+            yield transaction
 
 transactions = [{
     "id": 939719570,
@@ -64,3 +38,32 @@ transactions = [{
         "from": "Счет 19708645243227258542",
         "to": "Счет 75651667383060284188"
     }]
+
+
+usd_transactions = filter_by_currency(transactions, "USD")
+for _ in range(2):
+    print(next(usd_transactions))
+
+
+
+
+def transaction_descriptions(list_of_transactions: Union[list, dict]) -> None:
+    """генератор, который принимает список словарей с транзакциями и возвращает описание каждой операции по очереди."""
+    for id_transaction in list_of_transactions:
+        print(id_transaction['id'])
+        yield id_transaction['description']
+
+descriptions = transaction_descriptions(transactions)
+for _ in range(5):
+        print(next(descriptions))
+
+def card_number_generator(start, end):
+    for i in range(start, end + 1):
+        card_number = '{:0>16}'.format(str(i))
+        card_number = ' '.join([card_number[i:i + 4] for i in range(0, len(card_number), 4)])
+        yield card_number
+
+for card_number in card_number_generator(1, 5):
+    print(card_number)
+
+
